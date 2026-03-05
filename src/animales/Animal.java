f498e52eaeae9b4e0f1e5f557c4bf5d55bc96200
Rafael <rafa.lopez.iniesta98@gmail.com>
2026-03-05 15:33:01 +0100

    Añadido JavaDoc completo clase Animal

--- src/animales/Animal.java
+++ src/animales/Animal.java
@@ -23,7 +23,7 @@ public abstract class Animal {
  * 
  * Realiza validaciones sobre los parámetros recibidos:
  * - El código debe tener exactamente 5 caracteres alfanuméricos en minúscula.
- * - El sexo debe ser 'M' (hembra) o 'H' (macho).
+ * - El sexo debe ser 'M' (femenino) o 'H' (masculino).
  * - El peso debe ser mayor que 0.
  * - La fecha de nacimiento debe tener formato ISO yyyy-MM-dd.
  * 
@@ -54,12 +54,21 @@ public abstract class Animal {
             this.sexo = sexo;
             this.peso = peso;
         }
-    }
-
+    }/**
+ * Devuelve el código identificativo del animal.
+ *
+ * @return código identificativo (5 caracteres alfanuméricos en minúscula)
+ */
     public String getCodigo() {
         return codigo;
     }
-
+/**
+ * Modifica el código identificativo del animal.
+ * El código debe contener exactamente 5 caracteres alfanuméricos en minúscula.
+ *
+ * @param codigo nuevo código identificativo
+ * @throws IllegalArgumentException si el código no cumple el patrón [0-9a-z]{5}
+ */
     public void setCodigo(String codigo) {
         if (!codigo.matches("[0-9a-z]{5}")) {
             throw new IllegalArgumentException();
@@ -67,11 +76,22 @@ public abstract class Animal {
             this.codigo = codigo;
         }
     }
+/**
+ * Devuelve la fecha de nacimiento del animal.
+ *
+ * @return fecha de nacimiento como {@link LocalDate}
+ */
 
     public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
-
+/**
+ * Modifica la fecha de nacimiento del animal.
+ * La fecha debe tener formato ISO-8601 (yyyy-MM-dd) y ser válida.
+ *
+ * @param fechaNacimiento fecha en formato yyyy-MM-dd
+ * @throws IllegalArgumentException si la fecha no tiene un formato válido
+ */
     public void setFechaNacimiento(String fechaNacimiento) {
         LocalDate fecha;
 
@@ -83,11 +103,21 @@ public abstract class Animal {
 
         this.fechaNacimiento = fecha;
     }
+/**
+ * Devuelve el sexo del animal.
+ *
+ * @return sexo del animal ('M' o 'H')
+ */
 
     public char getSexo() {
         return sexo;
     }
-
+/**
+ * Modifica el sexo del animal.
+ *
+ * @param sexo nuevo sexo del animal ('M' o 'H')
+ * @throws IllegalArgumentException si el sexo no es 'M' ni 'H'
+ */
     public void setSexo(char sexo) {
         if ((sexo != 'M' && sexo != 'H')) {
             throw new IllegalArgumentException();
@@ -95,7 +125,12 @@ public abstract class Animal {
             this.sexo = sexo;
         }
     }
-
+/**
+ * Modifica el peso del animal.
+ *
+ * @param peso nuevo peso en kilogramos, debe ser mayor que 0
+ * @throws IllegalArgumentException si el peso es menor o igual que 0
+ */
     public double getPeso() {
         return peso;
     }
@@ -107,7 +142,11 @@ public abstract class Animal {
             this.peso = peso;
         }
     }
-
+/**
+ * Calcula el código hash del animal a partir de sus atributos.
+ *
+ * @return valor hash del objeto
+ */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -117,7 +156,14 @@ public abstract class Animal {
         hash = 19 * hash + (int) (Double.doubleToLongBits(this.peso) ^ (Double.doubleToLongBits(this.peso) >>> 32));
         return hash;
     }
-
+/**
+ * Compara este animal con otro objeto para determinar si son iguales.
+ * Se considera que dos animales son iguales si tienen los mismos valores
+ * en sus atributos.
+ *
+ * @param obj objeto con el que se compara
+ * @return true si son iguales; false en caso contrario
+ */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -139,23 +185,44 @@ public abstract class Animal {
         if (!Objects.equals(this.codigo, other.codigo)) {
             return false;
         }
-        if (!Objects.equals(this.fechaNacimiento, other.fechaNacimiento)) {
-            return false;
-        }
-        return true;
+        return Objects.equals(this.fechaNacimiento, other.fechaNacimiento);
     }
-
+/**
+ * Devuelve una representación en forma de texto del animal.
+ *
+ * @return cadena con los datos del animal
+ */
     @Override
     public String toString() {
         return "Animal{" + "codigo=" + codigo + ", fechaNacimiento=" + fechaNacimiento + ", sexo=" + sexo + ", peso=" + peso + '}';
     }
-
+/**
+ * Devuelve el sonido característico del animal.
+ *
+ * @return sonido del animal
+ *//**
+ * Devuelve el sonido característico del animal.
+ *
+ * @return sonido del animal
+ */
     public abstract String hacerSonido();
-
+/**
+ * Devuelve el mensaje/acción que representa que el animal se alegra.
+ *
+ * @return mensaje de alegría del animal
+ */
     public abstract String alegrarse();
-
+/**
+ * Devuelve el mensaje/acción que representa que el animal se enfada.
+ *
+ * @return mensaje de enfado del animal
+ */
     public abstract String enfadarse();
-
+/**
+ * Devuelve el tipo de animal (por ejemplo: perro, gato, etc.).
+ *
+ * @return descripción del animal
+ */
     public abstract String queSoy();
 
 }
